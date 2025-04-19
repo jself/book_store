6f7cf6580909aa078eb24402a9ed4d34195a52f7
Jeremy Self <jeremy.self@fiveq.com>
2025-04-18 21:48:51 -0500

    Initial phoenix project

---
+++ config/test.exs
@@ -0,0 +1,34 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :book_store, BookStore.Repo,
+  database: Path.expand("../book_store_test.db", __DIR__),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :book_store, BookStoreWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "fHFyn7+yvl+JnatzkqfvZNzeFDI5St6ZaYnfjXarC5owEWe6jMGieBT/A/Vp0nfn",
+  server: false
+
+# In test we don't send emails
+config :book_store, BookStore.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
